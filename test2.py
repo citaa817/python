5f2ae1b8fef6868b1c8f46c96334ad401bf0a810
citaa <citaa817@gmail.com>
2024-10-28 22:18:53 +0330

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,27 @@
+name= input()
+family=input()
+shimi=float(input())
+farsi=float(input())
+arbi=float(input())
+
+if shimi>=17:
+    print(shimi,"great")
+elif 17>shimi>=12:
+    print(shimi,"normal")
+elif shimi <12:
+    print(shimi,"fail")
+
+if farsi>=17:
+    print(farsi,"great")
+elif 17>farsi>=12:
+    print(farsi,"normal")
+elif farsi <12:
+    print(farsi,"fail")
+
+
+if arbi>=17:
+    print(arbi,"great")
+elif 17>arbi>=12:
+    print(arbi,"normal")
+elif shimi <12:
+    print(arbi,"fail")
